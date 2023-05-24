228722856b90ea7aff1512c1f1306a2a73b30f14
Sevagen Veerasamy <sevagenv@gmail.com>
2023-05-24 20:32:05 +0400

    [fix] Fix wrong date on meeting card

--- src/dashboard/Card.jsx
+++ src/dashboard/Card.jsx
@@ -34,7 +34,8 @@ const Card = ({
   }
 
   // startDate : Number Month (1 January);
-  let startDate = `${new Date(item.start).getDay()} ${months[new Date(item.start).getMonth()]}`
+  let startDate = `${new Date(item.start).getDate()} ${months[new Date(item.start).getMonth()]}`
+  console.log("StartDate", startDate, item.start, item);
 
   // startTime: 11:00 AM
   let startTime = `${new Date(item.start).getHours()}:${(new Date(item.start).getMinutes() < 10 ? "0":"") + new Date(item.start).getMinutes()} ${timeConvention(new Date(item.start).getHours())}`;
