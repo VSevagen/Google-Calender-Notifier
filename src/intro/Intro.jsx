d4f3460fdd168f11fac78947b3ffd5e486781657
Sevagen Veerasamy <sevagenv@gmail.com>
2023-05-29 19:20:02 +0400

    [feat] ADD intro component

--- src/intro/Intro.jsx
+++ src/intro/Intro.jsx
@@ -6,26 +6,34 @@ const Intro = () => {
 
   const { theme } = React.useContext(ThemeContext);
   const [tokenPresent] = React.useState(window?.localStorage?.getItem('access_token'));
-  
+
   return (
     <>
     {!tokenPresent &&
-      <button 
-        className={`rounded py-[10px]
-          ${theme === 'light' ? "bg-[#fff]" : "bg-[#27374D]"}
-          ${theme === 'light' ? "text-[#444]" : "text-white"}
-          p-2
-          shadow-[1px_1px_1px_grey] border-[1px] border-[#888] hover:opacity-[0.6]`}
-        onClick={
-          () => {
-            if(window && window?.activateSignIn) {
-              window.activateSignIn();
+      <div className={`text-[#f7f7ff] mx-4 sm:w-1/2 sm:mx-auto mt-[50px] sm:mt-[100px] bg-[#131112] px-3 py-5 rounded-[10px]`}>
+        <ol>
+          <li className="py-2">Sign in using your gmail account to access your meetings</li>
+          <li className="py-2">In case, you're not able to sign in, please send an email to <strong>sevagenv@gmail.com</strong> with your primary email address</li>
+        </ol>
+        <button
+          className={`
+            rounded mt-[5px]
+            ${theme === 'light' ? "bg-[#fff]" : "bg-[#27374D]"}
+            ${theme === 'light' ? "text-[#444]" : "text-white"}
+            p-2
+            shadow-[1px_1px_1px_grey] border-[1px] border-[#888] hover:opacity-[0.6]`}
+          onClick={
+            () => {
+              if(window && window?.activateSignIn) {
+                window.activateSignIn();
+              }
             }
-          }
-        }>
-        <img className="w-[25px] h-[25px] icon inline mr-[10px]" src={Search}></img>
-        <span className="buttonText">Sign in with Google</span>
-      </button>}
+          }>
+          <img className="w-[25px] h-[25px] icon inline mr-[10px]" src={Search}></img>
+          <span className="buttonText">Sign in with Google</span>
+        </button>
+      </div>
+      }
     </>
   )
 }
